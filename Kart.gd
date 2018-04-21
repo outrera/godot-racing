397f70587b0bcb8225b82f6aff3247b1ae9c63e2
Dvlv <dvd292@googlemail.com>
2018-04-21 12:08:12 +0100

    accel and deccel

--- Kart.gd
+++ Kart.gd
@@ -1,42 +1,31 @@
 extends KinematicBody2D
 
-var direction = Vector2()
 var speed = 0
 var acceleration = 5
-var decceleration = 10
+var decceleration = 3
 
-var motion = Vector2()
-var target_motion =  Vector2()
-var steering = Vector2()
-var target_angle = 0
+const MAX_SPEED = 280
 
-var skin = null
-
-const MASS = 2
-const MAX_SPEED = 60
-
-
-func _ready():
-	# Called every time the node is added to the scene.
-	# Initialization here
-	skin = $Sprite
 
 func _physics_process(delta):
-	
-	# The input works the same way as in the previous top-down demo. We reset the direction to (0, 0) on every frame and modify the vector based on the input. With 4 keys supported, you get 8 possible directions.
 	var is_pressing_move = false
 
 	if Input.is_action_pressed("ui_up"):
 		speed += acceleration
 		is_pressing_move = true
+		speed = clamp(speed, min(speed, 0), MAX_SPEED)
 	elif Input.is_action_pressed("ui_down"):
 		speed -= decceleration
 		is_pressing_move = true
+		speed = clamp(speed, -MAX_SPEED, max(speed, 0))
 		
 	if not is_pressing_move:
-		speed -= decceleration/2
-		
-	speed = clamp(speed, 0, MAX_SPEED)
+		if speed > 0:
+			speed -= decceleration/2
+			speed = clamp(speed, 0, MAX_SPEED)
+		else:
+			speed += decceleration/2
+			speed = clamp(speed, -MAX_SPEED, 0)
 
 	if Input.is_action_pressed("ui_left"):
 		self.rotation -= 0.05
@@ -46,7 +35,6 @@ func _physics_process(delta):
 	var move = get_transform().x
 	
 	var motion = move * speed
-	print(motion)
 	
 	move_and_slide(motion)
 	
