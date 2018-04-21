a52d877b5aa1f36156e73fdb8764de0fd9972ba5
Dvlv <dvd292@googlemail.com>
2018-04-21 12:26:47 +0100

    game

--- Kart.gd
+++ Kart.gd
@@ -5,6 +5,7 @@ var acceleration = 5
 var decceleration = 3
 
 const MAX_SPEED = 280
+const FRICTION = 10
 
 
 func _physics_process(delta):
@@ -12,19 +13,24 @@ func _physics_process(delta):
 
 	if Input.is_action_pressed("ui_up"):
 		speed += acceleration
+		if speed < 0:
+			speed += FRICTION
+			
 		is_pressing_move = true
 		speed = clamp(speed, min(speed, 0), MAX_SPEED)
 	elif Input.is_action_pressed("ui_down"):
 		speed -= decceleration
+		if speed > 0:
+			speed -= FRICTION
 		is_pressing_move = true
 		speed = clamp(speed, -MAX_SPEED, max(speed, 0))
 		
 	if not is_pressing_move:
 		if speed > 0:
-			speed -= decceleration/2
+			speed -= FRICTION
 			speed = clamp(speed, 0, MAX_SPEED)
 		else:
-			speed += decceleration/2
+			speed += FRICTION
 			speed = clamp(speed, -MAX_SPEED, 0)
 
 	if Input.is_action_pressed("ui_left"):
